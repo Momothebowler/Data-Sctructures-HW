90276ac207eb97cf81e3a02d1e53aed0b0bd0245
Momothebowler <momothebowler@gmail.com>
2024-11-13 11:22:06 -0500

    Hw8Done?

--- HW08.cpp
+++ HW08.cpp
@@ -2,33 +2,34 @@
 #include <iostream>
 #include <list>
 #include "timer.h"
-#include <string>
-#include <iomanip>
+// #include <iomanip>
 
 int main()
 {
     Timer<> clock;
     const int runs = 100;
-    clock.tick();
     std::list<double> arr;
+    double secs = 0;
+    double secs2 = 0;
     for (int i = 0; i <= 1000000; i++)
         arr.push_back(i);
 
     for (int i = 0; i < runs; i++)
     {
+        clock.tick();
         double sum = 0;
         for (double item : arr)
             sum += item;
 
         // std::cout << "Sum is: " << std::setprecision(15) << sum << "\n";
         sum = 0;
+        clock.tock();
+        secs += clock.duration().count() / 1000.0;
     }
-    clock.tock();
 
-    const double secs = clock.duration().count() / 1000.0 / runs;
+    secs = secs / runs;
     std::cout << "Took:" << secs << " seconds\n";
 
-    clock.tick();
     std::vector<int> a(1000001);
 
     for (int i = 0; i <= 1000000; ++i)
@@ -36,19 +37,20 @@ int main()
 
     for (int i = 0; i < runs; i++)
     {
+        clock.tick();
         double sum = 0;
         for (double item : a)
             sum += item;
 
         // std::cout << "Sum is: " << std::setprecision(15) << sum << "\n";
         sum = 0;
+        clock.tock();
+        secs2 += clock.duration().count() / 1000.0;
     }
-    clock.tock();
-    const double secs2 = clock.duration().count() / 1000.0 / runs;
+
+    secs2 = secs2 / runs;
     std::cout << "Took:" << secs2 << " seconds\n";
-    // std::cout << "List uses: " << sizeof(arr.back()) << " bytes\n";
     //  lists uses 8 bytes per item
-    // std::cout << "Vector uses: " << sizeof(a[1000000 - 1]) << " bytes\n";
     //  Vectors use 4 bytes per item
     std::cout << "List uses: " << arr.size() * sizeof(arr.back()) << " bytes\n";
     std::cout << "Vector uses: " << a.size() * sizeof(a[1000000]) << " bytes\n";
